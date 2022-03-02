eb14b1d65f2ea6ce2d884039bdfa4a96df24ae58
gvasey <gvasey@umich.edu>
2022-03-01 22:54:46 -0500

    sphinx

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. WSINDy_PDE_Gina documentation master file, created by
+   sphinx-quickstart on Tue Mar  1 22:46:55 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to WSINDy_PDE_Gina's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- subsamp.m
+++ subsamp.m
@@ -1,20 +1,13 @@
-%> @file  subsamp.m
-%> @brief Subsampling data
-
-%> ======================================================================
-%> @brief Subsamples data U_obs and coordinates xs_obs according to coarsen_data
-%>
-%> Subsamples data U_obs and coordinates xs_obs according to coarsen_data
-%>
-%> @param xs_obs spatial locations of data as cell array of shape (1,D+1) so xs_obs{D} is grid points in last dimension D and xs_obs{D+1} is in time
-%> @param U_obs Observed data as cell array with shape (1,n) for data with n state variables
-%> @param coarsen_data Instructions on how to subsample, array of shape (D,3) coarsen_data(i,:) = [start_idx, increment, end_idx]
-%> @param dims dimensions of single variable of U_obs (assumed all same shape)
-%>
-%> @retval xs_obs coarsened spatial grid
-%> @retval U_obs coarsened data
-%> ======================================================================
 function [xs_obs,U_obs] = subsamp(xs_obs,U_obs,coarsen_data,dims)
+% Subsamples data U_obs and coordinates xs_obs according to coarsen_data
+%
+% :param xs_obs: spatial locations of data as cell array of shape (1,D+1) so xs_obs{D} is grid points in last dimension D and xs_obs{D+1} is in time
+% :param U_obs: Observed data as cell array with shape (1,n) for data with n state variables
+% :param coarsen_data: Instructions on how to subsample, array of shape (D,3) coarsen_data(i,:) = [start_idx, increment, end_idx]
+% :param dims: dimensions of single variable of U_obs (assumed all same shape)
+%
+% :returns: xs_obs coarsened spatial grid
+% :returns: U_obs coarsened data
     if ~and(all(reshape(coarsen_data(:,1:2)==1,[],1)),...
             all(coarsen_data(:,end)==dims'))
         nstates = length(U_obs);
